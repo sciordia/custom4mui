5660e8c915aedba98c7fa398f1e30f79a6c4b597
Sergio Ciordia <sciordia@cnb.csic.es>
2023-11-13 00:20:38 +0100

    Update R Folder

---
+++ R/examples.R
@@ -0,0 +1,18 @@
+#' Component example
+#'
+#' Used for documentation purposes to show shiny examples.
+#'
+#' @export
+#' @param ... ui taglist to render
+
+custom4mui_example <- function(...) {
+  if (interactive()) {
+    options(shiny.launch.browser = FALSE)
+
+    shiny::shinyApp(
+      ui = shiny::tagList(...),
+      server = function(input, output) {}
+    )
+  }
+
+}
